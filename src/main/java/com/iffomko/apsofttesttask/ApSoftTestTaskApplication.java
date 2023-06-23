31f8ea873b4791539ce794055aa5d7e9f72f16de
Ilya Fomko <Ilya.Fomko@urfu.me>
2023-06-23 19:02:17 +0500

    Initial commit

---
+++ src/main/java/com/iffomko/apsofttesttask/ApSoftTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.iffomko.apsofttesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApSoftTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApSoftTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffomko/apsofttesttask/ApSoftTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffomko.apsofttesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApSoftTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
